1e47ecee9d83dfd37f9f525107a1d5b5bc84ca88
AlivUV <alvaro.portocarrero@correounivalle.edu.co>
2025-04-08 11:03:53 -0500

    Display adjustments
    * Change appointment display order.
    
    * Apointments military time -> standard time.
    
    * User image stretched -> cropped and centered

--- src/app/_utils/imgConvert.ts
+++ src/app/_utils/imgConvert.ts
@@ -4,9 +4,22 @@ const imgToWebp = (file: any, setFileState: any) => {
   img.onload = () => {
     canvas.width = 200;
     canvas.height = 200;
-    canvas.getContext("2d")!.drawImage(img, 0, 0, 200, 200);
+    const minDimension = Math.min(img.width, img.height);
+    const newWidth = (img.width / minDimension) * 200;
+    const newHeight = (img.height / minDimension) * 200;
+    canvas
+      .getContext("2d")!
+      .drawImage(
+        img,
+        -(newWidth - 200) / 2,
+        -(newHeight - 200) / 2,
+        newWidth,
+        newHeight
+      );
     canvas.toBlob((blob: any) => {
-      const webpImg = new File([blob], file.name, { type: blob.type });
+      const webpImg = new File([blob], `${file.name.split(".")[0]}.webp`, {
+        type: blob.type,
+      });
       file = webpImg;
       file.url = URL.createObjectURL(webpImg);
       setFileState((actual: any) => file);
